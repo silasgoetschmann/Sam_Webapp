d330873be6365549270e73b944ab172ce58ec3b8
Goetschmann Silas <silas.goetschmann@ict-campus.net>
2025-06-06 10:38:49 +0200

    Initial commit

---
+++ AIAC_EnvApp-master/pages/index.js
@@ -0,0 +1,28 @@
+import styles from "@/styles/Home.module.css";
+import { Env } from "@humanwhocodes/env";
+
+export default function Home({ items }) {
+  return (
+    <>
+      <h1>Welcome</h1>
+      <div>
+        {items.map((item) => {
+          return <p key={item.id}>{item.text}</p>;
+        })}
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const env = new Env();
+
+  const res = await fetch(env.get("FUNC_URL"));
+
+  let items = await res.json();
+
+  return {
+    props: { items },
+    revalidate: 10,
+  };
+}
